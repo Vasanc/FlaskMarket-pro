6a39e7d21704ab5e7b3cf826562dbaf03dc551a3
Vasanc <vasanaids@smvec.ac.in>
2022-07-05 19:31:44 +0530

    commit

--- market.py
+++ market.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/about/<username>")
+def about(username):
+    return f"<h1>Hello, {username}!</h1>"
+
 if __name__=='__main__':
     app.run(port=4000)
