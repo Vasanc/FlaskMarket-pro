989b514ebabc9413e8050c06548e262cc3dca7bd
Vasanc <vasanaids@smvec.ac.in>
2022-07-13 09:39:40 +0530

    prject restructure

--- run.py
+++ run.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 from market import app
 
 if __name__=='__main__':
-=======
-from market import app
-
-if __name__=='__main__':
->>>>>>> e9cbf8a524323da5bfeb2007b5f0d6b9e785a732
     app.run(port=5000)
